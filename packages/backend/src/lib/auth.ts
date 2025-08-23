80b06887adfe3540afbec204aee05a1e94bfa79b
Julian Archila <julianarchilacaro@gmail.com>
2025-08-22 23:06:56 -0500

    Add google auth (#2)
    * Add google auth and user menu to the sidebar
    
    * oauth proxy plugin
    
    * minor
    
    * we will not use the oauth proxy :(

--- packages/backend/src/lib/auth.ts
+++ packages/backend/src/lib/auth.ts
@@ -2,7 +2,7 @@ import { convexAdapter } from "@convex-dev/better-auth";
 import { convex } from "@convex-dev/better-auth/plugins";
 import { betterAuth } from "better-auth";
 import { betterAuthComponent } from "../convex/auth";
-import { type GenericCtx} from "../convex/_generated/server";
+import { type GenericCtx } from "../convex/_generated/server";
 import { phoneNumber } from "better-auth/plugins";
 import { internal } from "../convex/_generated/api";
 
@@ -10,6 +10,7 @@ const getSiteUrl = () => {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
+
 
 export const createAuth = (ctx: GenericCtx) =>
   // Configure your Better Auth instance here
@@ -21,6 +22,12 @@ export const createAuth = (ctx: GenericCtx) =>
     // Simple non-verified email/password to get started
     emailAndPassword: {
       enabled: false,
+    },
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      },
     },
     plugins: [
       // The Convex plugin is required
