b4392d7f9c3083360a1a5551a725cd8c5efb10f2
Julian Archila <julianarchilacaro@gmail.com>
2025-09-01 18:09:41 -0500

    add PostHog integration and update configuration

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 import "@env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/relay-r5mr/static/:path*",
+                destination: "https://us-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/relay-r5mr/:path*",
+                destination: "https://us.i.posthog.com/:path*",
+            },
+        ];
+    },
+
+    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
