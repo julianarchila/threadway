f78df1882fab97dc482c26b73a69edeae1b2749a
Julian Archila <julianarchilacaro@gmail.com>
2025-09-11 09:47:35 -0500

    wip

--- packages/backend/src/convex/agent/actions.ts
+++ packages/backend/src/convex/agent/actions.ts
@@ -1,4 +1,5 @@
 "use node";
+import { stepCountIs } from "@convex-dev/agent";
 import { internalAction } from "../_generated/server";
 import type { DomainMessage } from "../twilio/normalizer";
 import { findOrCreateUser, findChatUser } from "../user/helpers";
@@ -7,6 +8,8 @@ import { getUserThread, agent, loadUserTools, sendErrorMessageToUser, sendMessag
 const landingPageUrl = "https://threadway.app"
 
 const welcomeMessage = `Welcome to Threadway! I'm your personal assistant here to help you manage tasks, answer questions, and automate your notes-to-self pad. To join the waitlist visist: ${landingPageUrl}`
+
+const systemPrompt = `You are Threadway, a helpful personal assistant that lives in whatsapp. Your users are busy professionals who want to get things done quickly and efficiently. You are actiona as a smart notes-to-self-pad, task manager, and information retriever. You can help with a wide range of tasks, from setting reminders and managing to-do lists to answering questions and providing information. Always be concise and to the point. You will have access to a list of tools that correspond to external  services the user has connected to their account.`
 
 export const runAgentAction = internalAction({
   handler: async (ctx, args: { message: DomainMessage }): Promise<void> => {
@@ -30,7 +33,12 @@ export const runAgentAction = internalAction({
     // run agent
     const { _id: threadId } = await getUserThread(ctx, user._id);
     const { thread } = await agent.continueThread(ctx, { threadId })
-    const result = await thread.generateText({ prompt: args.message.text });
+    const result = await thread.generateText({
+      prompt: args.message.text,
+      system: systemPrompt,
+      tools: toolsRes.value,
+      stopWhen: stepCountIs(10)
+    });
     console.log("🤖 Agent result:", result);
 
     await sendMessageToUser(ctx, args.message.from, result.text)
